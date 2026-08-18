53cedda092908e28c4725ef20fc46bdc9af485d5
mitchelle <mitchellewanjohi69@gmail.com>
2026-08-18 19:48:41 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Rest API- tis is a backend API that follows the four rules below
+# 1. It has to have a route
+# 2. It has to have a method (POST/GET/PUT/DELETE)
+# 3. It has to have a status code (200,201,403)
+# 4. It has to return data as JSON (key: value pairs)
+
+from flask import Flask, request, jsonify
+import json
+
+app = Flask (__name__)
+
+@app.route("/")
+def home():
+    if request.method == "GET":
+        data = {"Flask API" : "Version 1"}
+        return jsonify(data), 200
+    else:
+        error = {"Error" : "Method not allowed"}
+        return jsonify(error), 403
+
+app.run(debug=True)
